5f6d9ccabaf0bc9e637bb62a0aaf496ab9472420
Emir-name <noob15526@gmail.com>
2026-07-20 00:31:42 +0500

    Register mod keybinds on client initialization

--- src/main/java/com/impactvisuals/ImpactVisualsClient.java
+++ src/main/java/com/impactvisuals/ImpactVisualsClient.java
@@ -1,6 +1,7 @@
 package com.impactvisuals;
 
 import com.impactvisuals.client.config.ModConfig;
+import com.impactvisuals.client.config.ModKeybinds;
 import com.impactvisuals.client.event.ClientEventHandler;
 import com.impactvisuals.client.sound.ModSounds;
 import net.fabricmc.api.ClientModInitializer;
@@ -12,6 +13,7 @@ public class ImpactVisualsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ModConfig.get(); // load/create config on startup
+        ModKeybinds.register();
         ModSounds.register();
         ClientEventHandler.register();
         System.out.println("[ImpactVisuals] Client visual effects initialized.");
