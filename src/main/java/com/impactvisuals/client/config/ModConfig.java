0b6b4762cca60bf6e3c21875eb83f4928fe36681
Emir-name <noob15526@gmail.com>
2026-07-30 00:10:20 +0500

    Add jumpRingEnabled configuration option

--- src/main/java/com/impactvisuals/client/config/ModConfig.java
+++ src/main/java/com/impactvisuals/client/config/ModConfig.java
@@ -21,6 +21,7 @@ public class ModConfig {
     public boolean hitParticlesEnabled = true;
     public boolean targetHudEnabled = true;
     public boolean buildHelperEnabled = false;
+    public boolean jumpRingEnabled = false;
     public boolean targetHudDebugEnabled = false;
     public boolean damageNumbersEnabled = true;
     public boolean criticalFlashEnabled = true;
