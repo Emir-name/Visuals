c90c8848093d5ed6f15c63af2706954d50608da4
Emir-name <noob15526@gmail.com>
2026-08-14 15:40:34 +0500

    Refactor ModConfig to use hatIndex instead of chinaHatEnabled
    Replaced chinaHatEnabled boolean with hatIndex integer for configuration.

--- src/main/java/com/impactvisuals/client/config/ModConfig.java
+++ src/main/java/com/impactvisuals/client/config/ModConfig.java
@@ -21,7 +21,7 @@ public class ModConfig {
     public boolean hitParticlesEnabled = true;
     public boolean targetHudEnabled = true;
     public boolean markerEnabled = false;
-    public boolean chinaHatEnabled = false;
+    public int hatIndex = 0;
     public java.util.Map<String, Integer> hudOffsetX = new java.util.HashMap<>();
     public java.util.Map<String, Integer> hudOffsetY = new java.util.HashMap<>();
     public java.util.Map<String, Float> hudScale = new java.util.HashMap<>();
